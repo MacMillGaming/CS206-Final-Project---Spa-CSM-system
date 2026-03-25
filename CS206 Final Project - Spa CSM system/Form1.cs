a894bb5c7ef41f5e78c4b07626d75ae02e872f12
austi <austi@MACDADDY>
2026-03-25 11:31:09 -0400

    Add project files.

---
+++ CS206 Final Project - Spa CSM system/Form1.cs
@@ -0,0 +1,10 @@
+namespace CS206_Final_Project___Spa_CSM_system
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CS206 Final Project - Spa CSM system/Program.cs
@@ -0,0 +1,17 @@
+namespace CS206_Final_Project___Spa_CSM_system
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
