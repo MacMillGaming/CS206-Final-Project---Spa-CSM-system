de8b2794509647204081dd488631e32cebdf2da3
sarahamam1 <sarahamam0806@gmail.com>
2026-04-06 13:53:38 -0400

    beautify form

--- CS206 Final Project - Spa CSM system/Form1.Designer.cs
+++ CS206 Final Project - Spa CSM system/Form1.Designer.cs
@@ -40,86 +40,111 @@
             // 
             // cboNameLookup
             // 
+            cboNameLookup.BackColor = Color.White;
             cboNameLookup.FormattingEnabled = true;
-            cboNameLookup.Location = new Point(43, 86);
+            cboNameLookup.Location = new Point(23, 40);
+            cboNameLookup.Margin = new Padding(2, 1, 2, 1);
             cboNameLookup.Name = "cboNameLookup";
-            cboNameLookup.Size = new Size(636, 40);
+            cboNameLookup.Size = new Size(344, 23);
             cboNameLookup.TabIndex = 0;
             cboNameLookup.SelectedIndexChanged += cboNameLookup_SelectedIndexChanged;
             // 
             // lstAppt
             // 
+            lstAppt.BackColor = Color.White;
             lstAppt.FormattingEnabled = true;
-            lstAppt.Location = new Point(43, 187);
+            lstAppt.ItemHeight = 15;
+            lstAppt.Location = new Point(23, 88);
+            lstAppt.Margin = new Padding(2, 1, 2, 1);
             lstAppt.Name = "lstAppt";
-            lstAppt.Size = new Size(983, 356);
+            lstAppt.Size = new Size(531, 169);
             lstAppt.TabIndex = 1;
             // 
             // btnAddRemoveCust
             // 
-            btnAddRemoveCust.Location = new Point(1044, 86);
+            btnAddRemoveCust.BackColor = Color.Pink;
+            btnAddRemoveCust.Font = new Font("Mongolian Baiti", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAddRemoveCust.Location = new Point(562, 40);
+            btnAddRemoveCust.Margin = new Padding(2, 1, 2, 1);
             btnAddRemoveCust.Name = "btnAddRemoveCust";
-            btnAddRemoveCust.Size = new Size(281, 65);
+            btnAddRemoveCust.Size = new Size(151, 30);
             btnAddRemoveCust.TabIndex = 2;
             btnAddRemoveCust.Text = "Add/Remove &Customer";
-            btnAddRemoveCust.UseVisualStyleBackColor = true;
+            btnAddRemoveCust.UseVisualStyleBackColor = false;
             btnAddRemoveCust.Click += btnAddRemoveCust_Click;
             // 
             // btnAddRemoveAppt
             // 
-            btnAddRemoveAppt.Location = new Point(1044, 209);
+            btnAddRemoveAppt.BackColor = Color.Pink;
+            btnAddRemoveAppt.Font = new Font("Mongolian Baiti", 9.75F);
+            btnAddRemoveAppt.Location = new Point(562, 98);
+            btnAddRemoveAppt.Margin = new Padding(2, 1, 2, 1);
             btnAddRemoveAppt.Name = "btnAddRemoveAppt";
-            btnAddRemoveAppt.Size = new Size(281, 65);
+            btnAddRemoveAppt.Size = new Size(151, 30);
             btnAddRemoveAppt.TabIndex = 3;
             btnAddRemoveAppt.Text = "Add/Remove &Appt";
-            btnAddRemoveAppt.UseVisualStyleBackColor = true;
+            btnAddRemoveAppt.UseVisualStyleBackColor = false;
             btnAddRemoveAppt.Click += btnAddRemoveAppt_Click;
             // 
             // btnViewSummaries
             // 
-            btnViewSummaries.Location = new Point(1044, 337);
+            btnViewSummaries.BackColor = Color.Pink;
+            btnViewSummaries.BackgroundImageLayout = ImageLayout.Stretch;
+            btnViewSummaries.Cursor = Cursors.IBeam;
+            btnViewSummaries.Font = new Font("Mongolian Baiti", 9.75F);
+            btnViewSummaries.ForeColor = SystemColors.ActiveCaptionText;
+            btnViewSummaries.Location = new Point(562, 158);
+            btnViewSummaries.Margin = new Padding(2, 1, 2, 1);
             btnViewSummaries.Name = "btnViewSummaries";
-            btnViewSummaries.Size = new Size(281, 66);
+            btnViewSummaries.Size = new Size(151, 31);
             btnViewSummaries.TabIndex = 4;
             btnViewSummaries.Text = "View &Summaries";
-            btnViewSummaries.UseVisualStyleBackColor = true;
+            btnViewSummaries.UseVisualStyleBackColor = false;
             btnViewSummaries.Click += btnViewSummaries_Click;
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(1066, 475);
+            btnExit.BackColor = Color.Pink;
+            btnExit.BackgroundImageLayout = ImageLayout.Stretch;
+            btnExit.Font = new Font("Mongolian Baiti", 9.75F);
+            btnExit.Location = new Point(562, 214);
+            btnExit.Margin = new Padding(2, 1, 2, 1);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(259, 63);
+            btnExit.Size = new Size(139, 30);
             btnExit.TabIndex = 5;
             btnExit.Text = "&Exit";
-            btnExit.UseVisualStyleBackColor = true;
+            btnExit.UseVisualStyleBackColor = false;
             btnExit.Click += btnExit_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(38, 40);
+            label1.Font = new Font("Georgia", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(20, 19);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(188, 32);
+            label1.Size = new Size(112, 15);
             label1.TabIndex = 6;
             label1.Text = "Customer Name";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(43, 143);
+            label2.Location = new Point(23, 67);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(197, 32);
+            label2.Size = new Size(99, 15);
             label2.TabIndex = 7;
             label2.Text = "Appointment List";
             // 
             // Form1
             // 
             AcceptButton = btnAddRemoveCust;
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LavenderBlush;
             CancelButton = btnExit;
-            ClientSize = new Size(1352, 611);
+            ClientSize = new Size(728, 286);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(btnExit);
@@ -128,7 +153,6 @@
             Controls.Add(btnAddRemoveCust);
             Controls.Add(lstAppt);
             Controls.Add(cboNameLookup);
-            Margin = new Padding(6);
             Name = "Form1";
             Text = "Form1";
             Load += form1_Load;
